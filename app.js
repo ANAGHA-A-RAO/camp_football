c81595351c23d4f0ce87b7679d47995943cac692
ANAGHA A RAO <anagharao88922@gmail.com>
2020-10-24 00:06:59 +0530

    all interaction completed

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ window.onload=()=>{
     updateButtonText();
     updateScore();
     updateNames();
+    
 }
 selectTurn=()=>{
 turn=Math.round(Math.random())+1;
@@ -23,16 +24,62 @@ console.log(turn);
 };
 
 updateButtonText=()=>{
-button=document.getElementById("strike");
-console.log("button");
-button.textContent= `${turn===1?team1.name:team2.name} strike`;
-}
+    button=document.getElementById("strike");
+    console.log("button");
+    var result=document.getElementById("result");
+    result.style.visibility="";
+    console.log(result);
+    button.textContent= `${turn===1?team1.name:team2.name} Strike`;
+    if(team1.runs.length==6 && team2.runs.length==6){
+    button.remove();
+    result.textContent=team1.score===team2.score? `It's is Draw`: `${team1.score > team2.score? team1.name:team2.name} WINS`;
+    }
+    else{
+    turn=team1.runs.length === 6?2:team2.runs.length===6?1:turn;
+    }
+    }
+
 updateScore=()=>{
-    document.getElementById('score-t1').textContent = team1.score;
-    document.getElementById('score-t2').textContent = team2.score;
+    document.getElementById('team1-score').textContent = team1.score;
+    document.getElementById('team2-score').textContent = team2.score;
+    updateRuns();
 }
 updateNames=()=>{
 
     document.getElementById('team-1').textContent = team1.name;
     document.getElementById('team-2').textContent = team2.name;
+}
+var handelStrikeButtonClick=()=> {
+    console.log("button clicked");
+    var runs = score[Math.floor(Math.random()*score.length)];
+    console.log('runs');
+    runs=runs == 5? 'w':runs;
+    console.log(runs);
+    if (turn === 1) {
+        team1.runs.push(runs);
+        team1.score = calculatescore(team1.runs);
+        console.log(team1.score);
+    }
+    else {
+        team2.runs.push(runs);
+        team2.score = calculatescore(team2.runs);
+        console.log(team2.score);
+    }
+    updateButtonText();
+    updateScore();
+}
+var calculatescore=(runs)=>{
+    return runs.map(run =>{
+        return run =='w'?0:run;
+    }).reduce((total,runs)=>total+runs);
+}
+updateRuns=()=>{
+    var teamOneRunsElement=document.getElementById("score-t1").children;
+    var teamTwoRunsElement=document.getElementById("score-t2").children;
+    team1.runs.forEach((run,index)=>{
+        teamOneRunsElement[index].textContent=run;
+    })
+    team2.runs.forEach((run,index)=>{
+        teamTwoRunsElement[index].textContent=run;
+    })
 }
